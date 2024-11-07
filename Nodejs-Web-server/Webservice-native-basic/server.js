34779cce31807737bcd8a126cad053ee5b86103e
Alamsyah-WM <alamsyahmassie18@gmail.com>
2024-11-07 08:58:10 +0700

    Update server.js mats to Status line/Response status

--- Nodejs-Web-server/Webservice-native-basic/server.js
+++ Nodejs-Web-server/Webservice-native-basic/server.js
@@ -2,18 +2,20 @@ const http = require('http');
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
     
     const { url, method } = request;
 
     if(url === '/') {
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('</h1>Welcome to Home Page</h1>');
     } else {
+        response.statusCode = 400;
         response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
     }
     } else if(url === '/about'){
         if(method === 'GET') {
+            response.statusCode = 200;
             response.end('Welcome to about us page');
         } else if(method === 'POST') {
             let body = [];
@@ -25,12 +27,15 @@ const requestListener = (request, response) => {
             request.on('end', () => {
             body = Buffer.concat(body).toString();
             const {name} = JSON.parse(body);
+            response.statusCode = 200;
             response.end(`<h1>Hai, ${name}!</h1>`)
         });
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else {
+        response.statusCode = 404;
         response.end('<h1>Halaman tidak ditemukan');
     }
 
